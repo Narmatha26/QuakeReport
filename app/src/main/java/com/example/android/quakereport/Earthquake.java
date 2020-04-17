ec8b830c9b1abbccbdf4960cbfe693792cfe18f8
Narmatha <narmathamurugesanbe@gmail.com>
2020-04-17 09:51:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,26 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    private double mMagnitude;
+    private String mLocation;
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *  @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the city location of the earthquake
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     */
+   public Earthquake(double magnitude, String location, long timeInMilliseconds){
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+    }
+
+    public double getMagnitude(){ return mMagnitude;}
+
+    public String getLocation(){return mLocation; }
+
+    public long getTimeInMilliseconds() { return mTimeInMilliseconds; }
+}
